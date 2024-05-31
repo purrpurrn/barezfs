902dd2e280b044254bd5bfadc77909e5edeed4c9
purrpurr <142632643+purrpurrn@users.noreply.github.com>
2024-05-31 20:12:27 +0200

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -16,10 +16,10 @@
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  # networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "nixos"; # Define your hostname.
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
   # Set your time zone.
   # time.timeZone = "Europe/Amsterdam";
