f2483a47311a65a9087b3ad5614e2d64b0e81b00
purrpurr <142632643+purrpurrn@users.noreply.github.com>
2024-05-31 20:30:01 +0200

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -63,6 +63,7 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.nixos = {
      isNormalUser = true;
+     password = "nixos";
      extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
      packages = with pkgs; [
        neovim
