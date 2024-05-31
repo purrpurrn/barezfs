66901986ef3d3ad5613e31d218b687d79ca9ad2b
purrpurr <142632643+purrpurrn@users.noreply.github.com>
2024-05-31 20:22:03 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
 
   outputs = { self, nixpkgs }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux"
+      system = "x86_64-linux";
       modules = [
         ./configuration.nix
       ];
