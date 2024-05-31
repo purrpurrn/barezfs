1c5521c506e525627f5c1fda10933f3847b4fa09
purrpurr <142632643+purrpurrn@users.noreply.github.com>
2024-05-31 20:16:36 +0200

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux"
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
